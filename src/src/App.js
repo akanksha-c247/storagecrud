9ad2baf66dcbfb137c21331c4ef899b59a481136
akanksha-c247 <akanksha.k@chapter247.com>
2023-08-08 09:59:49 +0530

    added new code

---
+++ src/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
+import Users from './Comp/ReadUser/Users';
+import AddUser from './Comp/CreateUser/AddUser';
+import EditUser from './Comp/EditUser/EditUser';
+
+function App() {
+  return (
+    <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Users/>}/>
+        <Route path='/adduser' element={<AddUser/>}/>
+        <Route path="/edituser/:id" element={<EditUser />} />
+      </Routes>
+    </BrowserRouter>
+    </div>
+
+  );
+}
+export default App;
